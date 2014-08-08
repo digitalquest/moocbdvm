810bed5e7ef47f59c9a603c757559a6043a3b2d5
Olivier Berger <olivier.berger@telecom-sudparis.eu>
2014-08-08 14:31:48 +0200

    Add cleanup

--- VagrantBox/DebianJessieBaseBox/setup-debian/provisioning.sh
+++ VagrantBox/DebianJessieBaseBox/setup-debian/provisioning.sh
@@ -52,46 +52,4 @@ apt-get install --yes virtualbox-guest-utils linux-headers-amd64 linux-image-amd
 apt-get dist-upgrade -q --yes --force-yes
 
 
-# # Install Apache web server, PGSQL, PHP, JS
-# echo ".installing needed applications"
-# apt-get install -q -y apache2 php5 postgresql libapache2-mod-php5 php5-pgsql javascript-common phppgadmin
-
-# apt-get clean
-
-# # Fetch the SQL scripts (acces: ssh germai_s@ssh)
-# # wget http://www-tp/~germai_s/script-vins.sql .
-# # wget http://www-tp/~germai_s/script-film.sql .
-# # wget http://www-tp/~germai_s/execpostgres.php .
-# # wget http://www-tp/~germai_s/interp.html .
-
-# # Create and mange the databases
-# sudo -u postgres psql -f setup.sql
-
-
-# # Get the path to pg_hba.conf
-# sudo -u postgres psql -c "SHOW hba_file" > config.txt
-
-# PathToConf=$(grep '/[a-z]*' config.txt)  
-
-# # Change authentification method from ident to trust
-# sed -ire 's/local [ \t]*all [ \t]*all [ \t]*peer/local all all trust/' $PathToConf
-
-# # Cleaning-up
-# rm config.txt
-
-# # Tell apache to read from conf.d
-# # sed -i '$a\Include conf.d/' > /etc/apache2/apache2.conf
-# ln -s /etc/apache2/conf.d/phppgadmin /etc/apache2/conf-enabled/phppgadmin.conf
-
-# # Change permission to access phppgadmin from all
-# sed -ire 's/deny from all/# deny from all/' /etc/apache2/conf.d/phppgadmin
-# sed -ire 's/allow from 127.0.0.0/# allow from 127.0.0.0/' /etc/apache2/conf.d/phppgadmin
-# sed -ire 's/# allow from all/allow from all/' /etc/apache2/conf.d/phppgadmin
-
-
-# # Restart postgresql services
-# /etc/init.d/postgresql reload
-
-# # Restart apache server
-# /etc/init.d/apache2 restart 
-
+apt-get clean
