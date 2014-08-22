c673605cb37961de94d923d91f2e9e0819e0bdaa
Olivier Berger <olivier.berger@telecom-sudparis.eu>
2014-08-22 12:57:52 +0200

    cleanup of commented old stuff

--- VagrantBox/Provisionning/setup-debian/setup-tp.sh
+++ VagrantBox/Provisionning/setup-debian/setup-tp.sh
@@ -9,13 +9,6 @@ mkdir -p /vagrant/TP
 chown -R vagrant:vagrant /vagrant/TP
 
 # Create tp.conf in sites-available with the configuration lines for apache to take it into account
-# touch /etc/apache2/sites-available/tp.conf
-# echo "Alias /TP /vagrant/TP" > /etc/apache2/sites-available/tp.conf
-# echo "<Directory /vagrant/TP>" >> /etc/apache2/sites-available/tp.conf
-# echo "Options Indexes FollowSymLinks" >> /etc/apache2/sites-available/tp.conf
-# echo "AllowOverride None" >> /etc/apache2/sites-available/tp.conf
-# echo "Require all granted" >> /etc/apache2/sites-available/tp.conf
-# echo "</Directory>" >> /etc/apache2/sites-available/tp.conf
 cat >/etc/apache2/sites-available/tp.conf <<EOF
 Alias /TP /vagrant/TP
 
